441fd26e4eca1f711f90b1cf74be5827caadbd39
FrancescoZinnato <147512111+FrancescoZinnato@users.noreply.github.com>
2025-02-03 12:47:01 +0100

    Adding JPA annotations, creating repos and repo tests

---
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring6restmvc.repositories;
+
+import guru.springframework.spring6restmvc.entities.Beer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest // Controllers and components will not be imported, simply a JPA test env
+class BeerRepositoryTest {
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testSaveBeer() {
+        Beer savedBeer = beerRepository.save(Beer.builder().beerName("Saved Beer").build());
+
+        assertThat(savedBeer).isNotNull();
+        assertThat(savedBeer.getId()).isNotNull();
+    }
+
+}
