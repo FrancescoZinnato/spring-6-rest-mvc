762745394aa62b71040680d5584a9b0dd54009f0
FrancescoZinnato <147512111+FrancescoZinnato@users.noreply.github.com>
2025-02-04 12:14:34 +0100

    patch return type update

--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,7 +18,14 @@ class BeerRepositoryTest {
 
     @Test
     void testSaveBeer() {
-        Beer savedBeer = beerRepository.save(Beer.builder().beerName("Saved Beer").build());
+        Beer savedBeer = beerRepository.save(Beer.builder()
+                .beerName("Saved Beer")
+                .beerStyle(BeerStyle.IPA)
+                .upc("2314123")
+                .price(BigDecimal.valueOf(4.99))
+                .build());
+
+        beerRepository.flush(); // Esegue l'operazione dopo aver creato correttamente l'oggetto, sennò fa troppo veloce e il test passa anche con dati errati
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
