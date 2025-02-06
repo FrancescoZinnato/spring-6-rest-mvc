09f8e24e513d6a03e311f3b424c7d48e1e1f5573
FrancescoZinnato <147512111+FrancescoZinnato@users.noreply.github.com>
2025-02-06 12:55:29 +0100

    Using @ServiceConnection to auto-config the container dynamics properties

--- src/test/java/guru/springframework/spring6restmvc/repositories/MySqlTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/MySqlTest.java
@@ -4,6 +4,7 @@ import guru.springframework.spring6restmvc.entities.Beer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -22,17 +23,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MySqlTest {
 
     @Container
+    @ServiceConnection // Configura il test container automaticamente e binda le proprietà dinamiche con quelle di springboot
     static MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8.3");
 
+    /*
     @DynamicPropertySource
     static void mysqlProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
         registry.add("spring.datasource.username", mysqlContainer::getUsername);
         registry.add("spring.datasource.password", mysqlContainer::getPassword);
     }
+    */
 
-    @Autowired
-    DataSource dataSource;
     @Autowired
     private BeerRepository beerRepository;
 
