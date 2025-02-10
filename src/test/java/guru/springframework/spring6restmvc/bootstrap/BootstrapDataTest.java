9f7b7b9d848a7399a5a23f2bfa1aaaa297241665
FrancescoZinnato <147512111+FrancescoZinnato@users.noreply.github.com>
2025-02-10 11:10:43 +0100

    Loading CSV data into the database with BootstrapData

--- src/test/java/guru/springframework/spring6restmvc/bootstrap/BootstrapDataTest.java
+++ src/test/java/guru/springframework/spring6restmvc/bootstrap/BootstrapDataTest.java
@@ -1,7 +1,38 @@
 package guru.springframework.spring6restmvc.bootstrap;
 
-import static org.junit.jupiter.api.Assertions.*;
+import guru.springframework.spring6restmvc.repositories.BeerRepository;
+import guru.springframework.spring6restmvc.repositories.CustomerRepository;
+import guru.springframework.spring6restmvc.services.BeerCsvService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 class BootstrapDataTest {
+
+    @Autowired
+    BeerRepository beerRepository;
+    @Autowired
+    CustomerRepository customerRepository;
+    @Autowired
+    BeerCsvService beerCsvService;
+
+    BootstrapData bootstrapData;
+
+    @BeforeEach
+    void setUp() {
+        bootstrapData = new BootstrapData(beerRepository, customerRepository, beerCsvService);
+    }
+
+    @Test
+    void Testrun() throws Exception {
+        bootstrapData.run(null);
+
+        assertThat(beerRepository.count()).isEqualTo(3);
+        assertThat(customerRepository.count()).isEqualTo(3);
+    }
 
 }
