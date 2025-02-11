f5dc3fab61818438503d4ed4b2e92da4a873b9fe
FrancescoZinnato <147512111+FrancescoZinnato@users.noreply.github.com>
2025-02-11 17:39:39 +0100

    Adding database scripts + adding new entity classes

--- src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
@@ -46,6 +46,7 @@ public class Beer {
     @NotNull
     private BigDecimal price;
     @CreationTimestamp
+    @Column(updatable = false)
     private LocalDateTime createdDate;
     @UpdateTimestamp
     private LocalDateTime updateDate;
