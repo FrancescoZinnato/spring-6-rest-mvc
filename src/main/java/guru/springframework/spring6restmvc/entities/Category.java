18f34414fd94087d51676175859160cebfd16a4e
FrancescoZinnato <147512111+FrancescoZinnato@users.noreply.github.com>
2025-02-12 12:52:50 +0100

    Repository + Test + adjustments to entities to maintain persistence

--- src/main/java/guru/springframework/spring6restmvc/entities/Category.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/Category.java
@@ -1,17 +1,16 @@
 package guru.springframework.spring6restmvc.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.*;
 import org.hibernate.type.SqlTypes;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
+@Builder
 @Getter
 @Setter
 @NoArgsConstructor
@@ -38,10 +37,11 @@ public class Category {
     @Version
     private Long version;
 
+    @Builder.Default
     @ManyToMany
     @JoinTable(name = "beer_category",
             joinColumns = @JoinColumn(name = "category_id"),
             inverseJoinColumns = @JoinColumn(name = "beer_id"))
-    private Set<Beer> beers;
+    private Set<Beer> beers = new HashSet<>();
 
 }
